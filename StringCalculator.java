35d5807a323217e2f2b7f19ce098c7a7536c2012
nirav <karathiyanirav@gmail.com>
2021-04-27 10:28:15 +0530

    Negative number will throw exception

--- StringCalculator.java
+++ StringCalculator.java
@@ -82,6 +82,58 @@ public class StringCalculator
         return returnValue;
     }
 
+    //Support different delimiters
+    public static int add(final String numbers) 
+    {
+        String delimiter = ",|n";
+        String numbersWithoutDelimiter = numbers;
+        if (numbers.startsWith("//")) 
+        {
+            int delimiterIndex = numbers.indexOf("//") + 2;
+            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("n") + 1);
+        }
+        return add(numbersWithoutDelimiter, delimiter);
+    }
+ 
+    private static int add(final String numbers, final String delimiter) 
+    {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        for (String number : numbersArray) 
+        {
+            if (!number.trim().isEmpty()) 
+            {
+                returnValue += Integer.parseInt(number.trim());
+            }
+        }
+        return returnValue;
+    }
 
+
+    //Negative numbers will throw an exception
+    private static int add(final String numbers, final String delimiter) 
+    {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        List negativeNumbers = new ArrayList();
+        for (String number : numbersArray) 
+        {
+            if (!number.trim().isEmpty()) 
+            {
+                int numberInt = Integer.parseInt(number.trim());
+                if (numberInt < 0) 
+                {
+                    negativeNumbers.add(numberInt);
+                }
+                returnValue += numberInt;
+            }
+        }
+        if (negativeNumbers.size() > 0) 
+        {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return returnValue;     
+    }
 
 }
--- test.java
+++ test.java
@@ -51,4 +51,35 @@ public class StringCalculatorTest {
          Assert.assertEquals(3+6+15, StringCalculator.add("3,6n15"));
     }
 
+     //Support different delimiters
+    @Test
+    public final void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() 
+    {
+        Assert.assertEquals(3+6+15, StringCalculator.add("//;n3;6;15"));
+    } 
+
+
+       
+    //Negative numbers will throw an exception
+    @Test(expected = RuntimeException.class)
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() 
+    {
+        StringCalculator.add("3,-6,15,18,46,33");
+    }
+    @Test
+    public final void whenNegativeNumbersAreUsedThenRuntimeExceptionIsThrown() 
+    {
+        RuntimeException exception = null;
+        try 
+        {
+            StringCalculator.add("3,-6,15,-18,46,33");
+        } 
+        catch (RuntimeException e) 
+        {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("Negatives not allowed: [-6, -18]", exception.getMessage());
+    }
+
 }
