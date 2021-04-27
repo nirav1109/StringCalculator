31568706c32d1273467986d731033547b2f275c2
nirav <karathiyanirav@gmail.com>
2021-04-27 09:52:13 +0530

    Test(1) - return sum if possible

---
+++ test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import com.wordpress.technologyconversations.tdd.StringCalculator;
+ 
+public class StringCalculatorTest {
+    @Test(expected = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,2,3");
+    }
+    @Test
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+        Assert.assertTrue(true);
+    }
+    @Test(expected = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
+    }
+}
